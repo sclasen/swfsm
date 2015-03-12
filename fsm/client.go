87e786a146b9329df2f66bbeb586a71519eefd50
Scott Clasen <scott@heroku.com>
2015-03-12 10:57:46 -0700

    fsm client

---
+++ fsm/client.go
@@ -0,0 +1,154 @@
+package fsm
+
+import (
+	"fmt"
+	"log"
+
+	"time"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/gen/swf"
+	"github.com/juju/errors"
+	. "github.com/sclasen/swfsm/sugar"
+)
+
+type FSMClient interface {
+	GetState(id string) (string, interface{}, error)
+	Signal(id string, signal string, input interface{}) error
+	Start(startTemplate swf.StartWorkflowExecutionInput, id string, input interface{}) (*swf.Run, error)
+}
+
+type ClientSWFOps interface {
+	ListOpenWorkflowExecutions(req *swf.ListOpenWorkflowExecutionsInput) (resp *swf.WorkflowExecutionInfos, err error)
+	ListClosedWorkflowExecutions(req *swf.ListClosedWorkflowExecutionsInput) (resp *swf.WorkflowExecutionInfos, err error)
+	GetWorkflowExecutionHistory(req *swf.GetWorkflowExecutionHistoryInput) (resp *swf.History, err error)
+	SignalWorkflowExecution(req *swf.SignalWorkflowExecutionInput) (err error)
+	StartWorkflowExecution(req *swf.StartWorkflowExecutionInput) (resp *swf.Run, err error)
+	TerminateWorkflowExecution(req *swf.TerminateWorkflowExecutionInput) (err error)
+	RequestCancelWorkflowExecution(req *swf.RequestCancelWorkflowExecutionInput) (err error)
+}
+
+func NewFSMClient(f *FSM, c ClientSWFOps) FSMClient {
+	return &client{
+		f: f,
+		c: c,
+	}
+}
+
+type client struct {
+	f *FSM
+	c ClientSWFOps
+}
+
+func (c *client) GetState(id string) (string, interface{}, error) {
+	var execution *swf.WorkflowExecution
+	open, err := c.c.ListOpenWorkflowExecutions(&swf.ListOpenWorkflowExecutionsInput{
+		Domain:          S(c.f.Domain),
+		MaximumPageSize: aws.Integer(1),
+		StartTimeFilter: &swf.ExecutionTimeFilter{OldestDate: &aws.UnixTimestamp{time.Unix(0, 0)}},
+		ExecutionFilter: &swf.WorkflowExecutionFilter{
+			WorkflowID: S(id),
+		},
+	})
+
+	if err != nil {
+		if ae, ok := err.(aws.APIError); ok {
+			log.Printf("component=client fn=GetState at=list-open error-type=%s message=%s", ae.Type, ae.Message)
+		} else {
+			log.Printf("component=client fn=GetState at=list-open error=%s", err)
+		}
+		return "", nil, err
+	}
+
+	if len(open.ExecutionInfos) > 0 {
+		execution = open.ExecutionInfos[0].Execution
+	} else {
+		closed, err := c.c.ListOpenWorkflowExecutions(&swf.ListOpenWorkflowExecutionsInput{
+			Domain:          S(c.f.Domain),
+			MaximumPageSize: aws.Integer(1),
+			StartTimeFilter: &swf.ExecutionTimeFilter{OldestDate: &aws.UnixTimestamp{time.Unix(0, 0)}},
+			ExecutionFilter: &swf.WorkflowExecutionFilter{
+				WorkflowID: S(id),
+			},
+		})
+
+		if err != nil {
+			if ae, ok := err.(aws.APIError); ok {
+				log.Printf("component=client fn=GetState at=list-closed error-type=%s message=%s", ae.Type, ae.Message)
+			} else {
+				log.Printf("component=client fn=GetState at=list-closed error=%s", err)
+			}
+			return "", nil, err
+		}
+
+		if len(closed.ExecutionInfos) > 0 {
+			execution = closed.ExecutionInfos[0].Execution
+		} else {
+			return "", nil, errors.Trace(fmt.Errorf("workflow not found for id %s", id))
+		}
+	}
+
+	history, err := c.c.GetWorkflowExecutionHistory(&swf.GetWorkflowExecutionHistoryInput{
+		Domain:       S(c.f.Domain),
+		Execution:    execution,
+		ReverseOrder: aws.True(),
+	})
+
+	if err != nil {
+		if ae, ok := err.(aws.APIError); ok {
+			log.Printf("component=client fn=GetState at=get-history error-type=%s message=%s", ae.Type, ae.Message)
+		} else {
+			log.Printf("component=client fn=GetState at=get-history error=%s", err)
+		}
+		return "", nil, err
+	}
+
+	serialized, err := c.f.findSerializedState(history.Events)
+
+	if err != nil {
+		log.Printf("component=client fn=GetState at=find-serialized-state error=%s", err)
+		return "", nil, err
+	}
+
+	data := c.f.zeroStateData()
+	err = c.f.Serializer.Deserialize(serialized.StateData, data)
+	if err != nil {
+		log.Printf("component=client fn=GetState at=deserialize-serialized-state error=%s", err)
+		return "", nil, err
+	}
+
+	return serialized.StateName, data, nil
+
+}
+
+func (c *client) Signal(id string, signal string, input interface{}) error {
+	var serializedInput aws.StringValue
+	if input != nil {
+		ser, err := c.f.Serializer.Serialize(input)
+		if err != nil {
+			return errors.Trace(err)
+		}
+		serializedInput = S(ser)
+	}
+	return c.c.SignalWorkflowExecution(&swf.SignalWorkflowExecutionInput{
+		Domain:     S(c.f.Domain),
+		SignalName: S(signal),
+		Input:      serializedInput,
+		WorkflowID: S(id),
+	})
+}
+
+func (c *client) Start(startTemplate swf.StartWorkflowExecutionInput, id string, input interface{}) (*swf.Run, error) {
+	var serializedInput aws.StringValue
+	if input != nil {
+		ser, err := c.f.Serializer.Serialize(input)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		serializedInput = S(ser)
+	}
+	startTemplate.Domain = S(c.f.Domain)
+	startTemplate.WorkflowID = S(id)
+	startTemplate.Input = serializedInput
+	return c.c.StartWorkflowExecution(&startTemplate)
+}
