03318deee81dec853a60a4a6395c8404857a82fd
Scott Clasen <scott@heroku.com>
2015-05-01 13:24:08 -0700

    more tests

--- activity/long_worker_test.go
+++ activity/long_worker_test.go
@@ -4,13 +4,18 @@ import (
 	"testing"
 
 	"time"
-
+    . "github.com/sclasen/swfsm/sugar"
 	"github.com/awslabs/aws-sdk-go/gen/swf"
 )
 
 func TestNewLongRunningActivityHandler(t *testing.T) {
-	handler := func(c *LongRunningActivityCoordinator, t *swf.ActivityTask, d *TestInput) {}
-	hc := NewHandleCoordinatedActivity(handler)
-	lrahf := NewCoordinatedActivityHandler(nil, 10*time.Second, 100, hc)
-	NewLongRunningActivityHandler("activity", lrahf)
+	hc := func(c *LongRunningActivityCoordinator, ta *swf.ActivityTask, d *TestInput) {
+		t.Log("HANDLE")
+		c.ToStopHeartbeating <- struct{}{}
+		<- c.ToAckStopHeartbeating
+	}
+	hca := NewHandleCoordinatedActivity(hc)
+	lrahf := NewCoordinatedActivityHandler(nil, 10*time.Second, 100, hca)
+	handler := NewLongRunningActivityHandler("activity", lrahf)
+	handler.HandlerFunc(&swf.ActivityTask{WorkflowExecution:&swf.WorkflowExecution{}, ActivityType: &swf.ActivityType{Name:S("name"),Version:S("ver")}}, &TestInput{})
 }
