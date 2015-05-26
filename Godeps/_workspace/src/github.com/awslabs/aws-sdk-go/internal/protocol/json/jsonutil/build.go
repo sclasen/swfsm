b002114ab965c6213b784da7b56f4659a82575ff
Scott Clasen <scott@heroku.com>
2015-05-26 10:16:06 -0700

    bump to working aws-sdk-go

--- Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/internal/protocol/json/jsonutil/build.go
+++ Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/internal/protocol/json/jsonutil/build.go
@@ -176,6 +176,16 @@ func writeString(s string, buf *bytes.Buffer) {
 	for _, r := range s {
 		if r == '"' {
 			buf.WriteString(`\"`)
+		} else if r == '\\' {
+			buf.WriteString(`\\`)
+		} else if r == '\b' {
+			buf.WriteString(`\b`)
+		} else if r == '\f' {
+			buf.WriteString(`\f`)
+		} else if r == '\r' {
+			buf.WriteString(`\r`)
+		} else if r == '\t' {
+			buf.WriteString(`\t`)
 		} else if r == '\n' {
 			buf.WriteString(`\n`)
 		} else if r < 32 {
