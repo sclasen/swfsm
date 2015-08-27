fbcb4bbf98f231a17406bc7bcee37a78e23f191c
Chris Brodt <chris@uberbrodt.net>
2015-08-27 15:28:36 -0500

    Make the mutable logger an interface
    This allows other log implementations to be subbed in

--- log/log.go
+++ log/log.go
@@ -5,6 +5,26 @@ import (
 	"os"
 )
 
+// Won't compile if StdLogger can't be realized by a log.Logger
+var _ StdLogger = &golog.Logger{}
+
+// StdLogger is what your logrus-enabled library should take, that way
+// it'll accept a stdlib logger and a logrus logger. There's no standard
+// interface, this is the closest we get, unfortunately.
+type StdLogger interface {
+	Print(...interface{})
+	Printf(string, ...interface{})
+	Println(...interface{})
+
+	Fatal(...interface{})
+	Fatalf(string, ...interface{})
+	Fatalln(...interface{})
+
+	Panic(...interface{})
+	Panicf(string, ...interface{})
+	Panicln(...interface{})
+}
+
 //provide a mutable logger so it can be changed
 //this is what the default logger in go's log pakcage looks like
-var Log = golog.New(os.Stderr, "", golog.LstdFlags)
+var Log StdLogger = golog.New(os.Stderr, "", golog.LstdFlags)
