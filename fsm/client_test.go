6cea2613ef49e6c721d1c62949a87e3d20d0aaf3
Ryan Brainard <brainard@heroku.com>
2015-10-09 23:28:12 -0700

    Add TestSegmentHistory

--- fsm/client_test.go
+++ fsm/client_test.go
@@ -556,8 +556,172 @@ func TestFindAll_FindLatestByWorkflowID(t *testing.T) {
 	mockSwf.AssertExpectations(t)
 }
 
+func TestSegmentHistory(t *testing.T) {
+	fsm := dummyFsm()
+
+	var readyData interface{}
+	readyData = TestData{States: []string{"ready data"}}
+
+	var startData interface{}
+	startData = TestData{States: []string{"start data"}}
+
+	workflowId := "workflow-id"
+	activityId := "activity-id"
+	activityData := "activity data"
+
+	itr := sliceHistoryIterator(t, []*swf.HistoryEvent{
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(7),
+			EventType: aws.String(swf.EventTypeActivityTaskScheduled),
+			ActivityTaskScheduledEventAttributes: &swf.ActivityTaskScheduledEventAttributes{
+				ActivityId: aws.String(activityId),
+				Input:      aws.String(fsm.Serialize(activityData)),
+				DecisionTaskCompletedEventId: aws.Int64(4),
+			},
+		},
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(6),
+			EventType: aws.String(swf.EventTypeMarkerRecorded),
+			MarkerRecordedEventAttributes: &swf.MarkerRecordedEventAttributes{
+				MarkerName: aws.String(CorrelatorMarker),
+				Details:    aws.String(fsm.Serialize(EventCorrelator{})),
+			},
+		},
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(5),
+			EventType: aws.String(swf.EventTypeMarkerRecorded),
+			MarkerRecordedEventAttributes: &swf.MarkerRecordedEventAttributes{
+				MarkerName: aws.String(StateMarker),
+				Details: aws.String(fsm.Serialize(SerializedState{
+					StateVersion: 1,
+					StateName:    "ready",
+					StateData:    fsm.Serialize(readyData),
+					WorkflowId:   workflowId,
+				})),
+			},
+		},
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(4),
+			EventType: aws.String(swf.EventTypeDecisionTaskCompleted),
+		},
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(3),
+			EventType: aws.String(swf.EventTypeDecisionTaskStarted),
+		},
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(2),
+			EventType: aws.String(swf.EventTypeDecisionTaskScheduled),
+		},
+		&swf.HistoryEvent{
+			EventId:   aws.Int64(1),
+			EventType: aws.String(swf.EventTypeWorkflowExecutionStarted),
+			WorkflowExecutionStartedEventAttributes: &swf.WorkflowExecutionStartedEventAttributes{
+				Input: StartFSMWorkflowInput(fsm, &startData),
+			},
+		},
+	})
+
+	actual, err := NewFSMClient(fsm, &mocks.SWFAPI{}).SegmentHistory(itr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []HistorySegment{
+		HistorySegment{
+			//			WorkflowId: aws.String(workflowId),
+			State: &HistorySegmentState{
+				ID:      aws.Int64(999999),
+				Version: uInt64(999999),
+				Name:    aws.String("<unrecorded>"),
+				Data:    nil,
+			},
+			Correlator: nil,
+			Events: []*HistorySegmentEvent{
+				&HistorySegmentEvent{
+					ID:   aws.Int64(7),
+					Type: aws.String(swf.EventTypeActivityTaskScheduled),
+					Attributes: &map[string]interface{}{
+						"ActivityId":                   activityId,
+						"ActivityType":                 nil,
+						"Control":                      nil,
+						"DecisionTaskCompletedEventId": 4,
+						"HeartbeatTimeout":             nil,
+						"Input":                        fsm.Serialize(activityData), // TODO: un-double escape?
+						"ScheduleToCloseTimeout":       nil,
+						"ScheduleToStartTimeout":       nil,
+						"StartToCloseTimeout":          nil,
+						"TaskList":                     nil,
+						"TaskPriority":                 nil,
+					},
+				},
+			},
+		},
+		HistorySegment{
+			WorkflowId: aws.String(workflowId),
+			State: &HistorySegmentState{
+				ID:      aws.Int64(5),
+				Version: uInt64(1),
+				Name:    aws.String("ready"),
+				Data:    &readyData,
+			},
+			Correlator: &EventCorrelator{},
+			Events: []*HistorySegmentEvent{
+				&HistorySegmentEvent{
+					ID:         aws.Int64(4),
+					Type:       aws.String(swf.EventTypeDecisionTaskCompleted),
+					References: []*int64{aws.Int64(7)},
+				},
+				&HistorySegmentEvent{
+					ID:   aws.Int64(3),
+					Type: aws.String(swf.EventTypeDecisionTaskStarted),
+				},
+				&HistorySegmentEvent{
+					ID:   aws.Int64(2),
+					Type: aws.String(swf.EventTypeDecisionTaskScheduled),
+				},
+			},
+		},
+		HistorySegment{
+			State: &HistorySegmentState{
+				ID:      aws.Int64(1),
+				Version: uInt64(0),
+				Name:    aws.String("initial"),
+				Data:    &startData,
+			},
+			Correlator: nil,
+			Events:     []*HistorySegmentEvent{},
+			WorkflowId: aws.String(""), // TODO: this is caused by a bug in initial serialization
+		},
+	}
+
+	for i, e := range expected {
+		a := actual[i]
+		if fsm.Serialize(e) != fsm.Serialize(a) {
+			t.Logf("expected[%d]:\n%#v\n\n actual[%d]:\n%#v", i, fsm.Serialize(e), i, fsm.Serialize(a))
+			t.FailNow()
+		}
+	}
+}
+
+func uInt64(v uint64) *uint64 {
+	return &v
+}
+
+func sliceHistoryIterator(t *testing.T, events []*swf.HistoryEvent) HistoryEventIterator {
+	cursor := 0
+	return func() (*swf.HistoryEvent, error) {
+		if cursor >= len(events) {
+			return nil, nil
+		}
+		event := events[cursor]
+		cursor++
+		t.Log(event)
+		return event, nil
+	}
+}
+
 func dummyFsm() *FSM {
-	return &FSM{
+	fsm := &FSM{
 		Domain:           "client-test",
 		Name:             "test-fsm",
 		DataType:         TestData{},
@@ -565,4 +729,12 @@ func dummyFsm() *FSM {
 		systemSerializer: JSONStateSerializer{},
 		allowPanics:      false,
 	}
+
+	fsm.AddInitialState(&FSMState{Name: "initial",
+		Decider: func(ctx *FSMContext, h *swf.HistoryEvent, data interface{}) Outcome {
+			return ctx.Stay(data, ctx.EmptyDecisions())
+		},
+	})
+
+	return fsm
 }
