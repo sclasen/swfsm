70d6deb80eaa1e657e2f5ffe2cdc98be2041ea81
Ryan Walls <ryan.walls@3dsim.com>
2016-07-20 15:23:43 -0700

    Make cancel channel in coordinated worker buffered

--- activity/coordinated_worker.go
+++ activity/coordinated_worker.go
@@ -86,7 +86,7 @@ func (c *coordinatedActivityAdapter) coordinate(activityTask *swf.PollForActivit
 		return nil, err
 	}
 
-	cancel := make(chan error)
+	cancel := make(chan error, 1)
 	stopHeartbeating := make(chan struct{})
 
 	go c.heartbeat(activityTask, stopHeartbeating, cancel)
