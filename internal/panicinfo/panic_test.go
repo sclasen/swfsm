e12f8c5891fab0056aaa83b975fc67a4b40efb11
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2020-06-11 15:44:17 +0900

    LocatePanic() line is best effort

--- internal/panicinfo/panic_test.go
+++ internal/panicinfo/panic_test.go
@@ -10,9 +10,15 @@ var line int
 
 func TestPanicHandler(t *testing.T) {
 	panicFunc()
-	// Line needs to match our panic call below!
-	if !strings.HasSuffix(file, "panic_test.go") || !strings.HasSuffix(name, "panicFunc") || line != 25 {
-		t.Errorf("Panic handler did not collect expected information: file=%s name=%s line=%d]", file, name, line)
+
+	// these must be correct
+	if !strings.HasSuffix(file, "panic_test.go") || !strings.HasSuffix(name, "panicFunc") {
+		t.Errorf("Panic handler did not collect expected required information: file=%s name=%s", file, name)
+	}
+
+	// these are best effort
+	if line != 31 {
+		t.Logf("Warning: Panic handler did not collect expected best-effort information: line=%d", line)
 	}
 }
 
